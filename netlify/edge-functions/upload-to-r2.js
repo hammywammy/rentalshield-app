2b02b9df02956a34ace0da1581746c5e800b3b79
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-07-02 17:14:37 -0700

    Update upload-to-r2.js

--- netlify/edge-functions/upload-to-r2.js
+++ netlify/edge-functions/upload-to-r2.js
@@ -6,7 +6,7 @@ export default async (request, context) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type',
-      },
+      }, 
     });
   }
 
